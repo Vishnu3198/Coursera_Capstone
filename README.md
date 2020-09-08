cee1c9805ed63b07f31112f477f01499eac2cfc7
vishnu3198 <vishnuteja901@gmail.com>
2020-09-08 09:57:34 +0530

    Final

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Coursera_Capstone
 First Data Science capstone project
-import pandas as pd
-import numpy as np
-print('Hello Capstone project course!')
--- scripts/data.py
+++ scripts/data.py
@@ -1,3 +1,5 @@
+# Coursera_Capstone1
+First Data Science capstone project
 import pandas as pd
 import numpy as np
 print('Hello Capstone project course!')
